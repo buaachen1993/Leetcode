bb81ff5b7b7701a8b6d9c7288d158e4db55ae88c
Jason <buaachen1993@gmail.com>
2016-01-20 19:16:06 +0800

    3 soloved

---
+++ src/problems/Add_Digits.java
@@ -0,0 +1,37 @@
+package problems;
+/***
+ * 258. Add Digits My Submissions Question
+ * @author chandler
+ *Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+ *For example:
+ *Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return it.
+ *Solution:
+ *https://en.wikipedia.org/wiki/Digital_root 258和292都是通过数学分析得到的结果。
+ */
+public class Add_Digits {
+	//数学方法
+    public int addDigits(int num) {
+    	
+        return 1+((num-1)%9);
+    }
+    //给出另外一种暴力的思路
+    public int addDigits2(int num){
+    	num = digitadd(num);
+    	while(num > 9)
+    	{
+    		num = digitadd(num);
+    	}
+    	return num;
+    }
+    public int digitadd(int num){
+    	String s = String.valueOf(num);
+    	int sum = 0;
+    	for(char ch:s.toCharArray())
+    	{
+    		sum += (ch - '0');
+    	}
+    	return sum;
+    }
+    
+
+}
--- src/problems/Nim_Game.java
+++ src/problems/Nim_Game.java
@@ -5,16 +5,18 @@ package problems;
  * You are playing the following Nim Game with your friend: 
  * There is a heap of stones on the table, each time one of you take turns to remove 1 to 3 stones. 
  * The one who removes the last stone will be the winner. You will take the first turn to remove the stones.
-Both of you are very clever and have optimal strategies for the game. 
-Write a function to determine whether you can win the game given the number of stones in the heap.
-For example, if there are 4 stones in the heap, then you will never win the game: 
-no matter 1, 2, or 3 stones you remove, the last stone will always be removed by your friend.
+ * Both of you are very clever and have optimal strategies for the game. 
+ * Write a function to determine whether you can win the game given the number of stones in the heap.
+ * For example, if there are 4 stones in the heap, then you will never win the game: 
+ * no matter 1, 2, or 3 stones you remove, the last stone will always be removed by your friend.
  * Solution:
+ * Impartial Combinatorial Games
+ * 每次两人加在一起取4个即可。
+ * 对于其他的nim 取子问题，判定亦或是否为0
  * 
  */
 public class Nim_Game {
 	public boolean canWinNim(int n) {
-		boolean canwin = false;
-		return canwin;
+		return n % 4 != 0;
 	}
 }
