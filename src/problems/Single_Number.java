7ca39194ec74fa1682ed7894f2fde4004c5dae91
Jason <buaachen1993@gmail.com>
2016-01-21 11:22:02 +0800

    4

---
+++ src/problems/Single_Number.java
@@ -0,0 +1,31 @@
+package problems;
+/***
+ * 136. Single Number 
+ * @author chandler
+ *Given an array of integers, every element appears twice except for one. Find that single one.
+ *Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+ *Solution:
+ *线性时间复杂度，不开额外空间
+ *1、最暴力方法就是从第一个数开始搜索
+ *2、额外开一个数组，标记次数，最后对额外数组扫一遍(哈希表)
+ *3、异或运算 a ^ b = b ^ a   0 ^ a = a  a ^ a = 0
+ */
+public class Single_Number {
+	public int singleNumber(int[] nums) {
+		int singlenumber = 0;
+		for (int i =0;i<nums.length;i++)
+		{
+			singlenumber = singlenumber ^ nums[i];
+		}
+			return singlenumber;
+	}
+	
+	public static void main(String args[])
+	{
+		 Single_Number sm = new Single_Number();
+		 int nums[] = {1,2,1,2,5,3,3};
+		 System.out.println(sm.singleNumber(nums));
+		
+	}
+
+}
---
+++ src/uitl/Hashtable_test.java
@@ -0,0 +1,24 @@
+package uitl;
+import java.util.Hashtable;
+
+public class Hashtable_test {
+	//哈希表测试
+	public static void main(String[] args) {
+		//key value
+		Hashtable<String, Integer> numbers = new Hashtable<String, Integer>();
+		//get 和put进行读取
+	    numbers.put("one", 1);
+	    numbers.put("two", 2);
+	    numbers.put("three", 3);
+	    numbers.put("four", 4);
+	    numbers.put("five", 5);
+
+	    Integer n = numbers.get("six");
+	    Integer nn = numbers.get("one");
+	        
+//	    if(n != null)
+//	    	System.out.println(n);
+//	    System.out.println(nn);
+	    System.out.println(1^1);
+	 }
+}
