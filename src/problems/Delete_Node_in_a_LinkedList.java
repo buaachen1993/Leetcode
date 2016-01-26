b781932c8cabd0ffc3bb6dd963057438b1c77324
Jason <buaachen1993@gmail.com>
2016-01-26 15:09:49 +0800

    invert binary tree

--- src/problems/Delete_Node_in_a_LinkedList.java
+++ src/problems/Delete_Node_in_a_LinkedList.java
@@ -3,7 +3,7 @@ package problems;
 import uitl.ListNode;
 
 /***
- * Delete Node in a Linked List
+ * 237 Delete Node in a Linked List
  * @author chandler
  *Write a function to delete a node (except the tail) in a singly linked list, given only access to that node.
  *Supposed the linked list is 1 -> 2 -> 3 -> 4 and you are given the third node with value 3, 
@@ -15,7 +15,7 @@ import uitl.ListNode;
  *     ListNode(int x) { val = x; }
  * }
  *Solution:
- * 给定的是要删除的节点 无法获取前一个节点 用后一个节点覆盖当前节点
+ * 给定的是要删除的节点 无法获取前一个节点 用后一个节点覆盖当前
  */
 public class Delete_Node_in_a_LinkedList {
     public void deleteNode(ListNode node) {
---
+++ src/problems/InvertBinaryTree.java
@@ -0,0 +1,41 @@
+package problems;
+
+import uitl.TreeNode;
+
+/***
+ * 226. Invert Binary Tree 
+ * @author chandler
+ * Invert a binary tree.
+ *  * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+to
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+
+ *Solution:递归问题？
+ */
+public class InvertBinaryTree {
+    public TreeNode invertTree(TreeNode root) {
+    	if(root == null)
+    	{
+    		return root;
+    	}
+    	TreeNode temp = root.left;
+    	root.left =  invertTree(root.right);
+    	root.right = invertTree(temp);
+    	return root;
+    }
+}
